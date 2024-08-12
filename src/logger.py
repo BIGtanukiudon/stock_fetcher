a975e74cd565cd14160e8cb05a43b9e5c768dcae
ryoK <test@example.com>
2024-08-12 21:28:56 +0900

    chore: logディレクトリにログファイルを出力する

--- src/logger.py
+++ src/logger.py
@@ -1,11 +1,12 @@
 import datetime
 import logging
+from os import mkdir, path
 
 # Create a logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-format = "%(levelname)-9s  %(asctime)s [%(filename)s:%(lineno)d] %(message)s"
+format = "%(asctime)s %(name)s [%(filename)s:%(lineno)d] %(levelname)s: %(message)s"
 
 st_handler = logging.StreamHandler()
 st_handler.setFormatter(logging.Formatter(format))
@@ -14,7 +15,13 @@ st_handler.setFormatter(logging.Formatter(format))
 datetime_string = datetime.datetime.now().strftime("%Y-%m-%d")
 
 # Create the file handler with the log file name
-fl_handler = logging.FileHandler(f"log_{datetime_string}.txt", encoding="utf-8")
+file_name = path.join("logs", f"log_{datetime_string}.log")
+
+# logDirectoryが存在しない場合は作成する
+if not path.exists("logs"):
+    mkdir("logs")
+
+fl_handler = logging.FileHandler(file_name, encoding="utf-8")
 
 # Create a formatter and add it to the handler
 formatter = logging.Formatter(format)
